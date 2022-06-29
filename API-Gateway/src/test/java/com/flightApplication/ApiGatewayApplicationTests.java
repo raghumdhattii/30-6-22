9d6a5ff030de889554b1770875ad06295a584c48
raghumdhattii <107978102+raghumdhattii@users.noreply.github.com>
2022-06-30 02:23:07 +0530

    apigatewayservice

---
+++ API-Gateway/src/test/java/com/flightApplication/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
